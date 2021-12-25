528807a41b91d800b816916acf96449bfb713c86
Saimon <saimon.ventura@freshbooks.com>
2021-12-25 09:45:25 -0300

    Hello, World! Swift

---
+++ Studies SV/Studies_SVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Studies_SVApp.swift
+//  Studies SV
+//
+//  Created by Saimon Ventura on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Studies_SVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
